12f70a9500b66c129d86ba02de7172fd34233db4
Shaina C <scailles23@gmail.com>
2020-01-14 05:44:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,15 @@
 # Make your shoe class here!
 
 class Shoe
+  attr_accessor :color, :brand, :material, :condition
+  attr_reader :brand
   
+  def initialize(brand)
+    @brand=brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def 
