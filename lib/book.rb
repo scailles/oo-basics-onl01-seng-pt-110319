098cb367fd63326169cff025659cf21ec44983d3
Shaina C <scailles23@gmail.com>
2020-01-14 06:04:54 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,36 +2,11 @@
 
 class Book
   
- def initialize(title)
-   @title=title
- end
- 
- def title
-   @title
- end
- 
- def author=(author)
-   @author=author
- end
- 
- def author
-   @author
- end
- 
- def page_count=(num)
-   @page_count=num
- end
- 
- def page_count
-   @page_count
- end
-  
-  def genre=(genre)
-    @genre=genre
-  end
-  
-  def genre
-    @genre
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+  def initialize(title)
+  @title=title
   end
   
   def turn_page
@@ -39,4 +14,3 @@ class Book
   end
   
 end
-
